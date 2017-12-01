26e63a322d0195e700597e2d898eedf2e5e4584b
potenbtlife <631938608@qq.com>
2017-12-01 17:56:33 +0800

    no zip compress.

--- ASTManager/CRTData.cpp
+++ ASTManager/CRTData.cpp
@@ -404,7 +404,7 @@ int CRTDataProxy::GetHttpData(string url, string& allGetData, FILE* flstm){
     httpHead += "Proxy-Connection: keep-alive\r\n";
     httpHead += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n";
     httpHead += "User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36\r\n";
-    //httpHead += "Accept-Encoding: gzip, deflate, sdch\r\n";
+    //httpHead += "Accept-Encoding: gzip, deflate, sdch\r\n"; 
 	httpHead += "Accept-Encoding: deflate, sdch\r\n";
     httpHead += "Accept-Language: zh-CN,zh;q=0.8,en;q=0.6\r\n\r\n";
 
